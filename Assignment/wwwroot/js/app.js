834092c9b34bea7d8ab4809097fc722321f65036
Ho Wai Xian <waixian321@gmail.com>
2025-12-11 01:54:03 +0800

    update layout, home page, css and js

---
+++ Assignment/wwwroot/js/app.js
@@ -0,0 +1,26 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    // --- Dark Mode  ---
+    const themeToggleBtn = document.getElementById('theme-toggle');
+    const themeIcon = themeToggleBtn.querySelector('.material-symbols-outlined');
+    const body = document.body;
+
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme === 'dark') {
+        body.classList.add('dark-mode');
+        themeIcon.textContent = 'light_mode'; 
+    }
+
+    themeToggleBtn.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('theme', 'dark');
+            themeIcon.textContent = 'light_mode'; 
+        } else {
+            localStorage.setItem('theme', 'light');
+            themeIcon.textContent = 'dark_mode'; 
+        }
+    });
+
+});
