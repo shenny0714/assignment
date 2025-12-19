3950deef78ead77ad4c1e3a659becc187f811739
Shenny <teexes-pm23@student.tarc.edu.my>
2025-12-19 18:16:32 +0800

    Clean repo, ignore build artifacts

--- Assignment/obj/Debug/net9.0/Assignment.AssemblyInfo.cs
+++ Assignment/obj/Debug/net9.0/Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf90c40780aed8fa6320e8249425d9d9e3b0322")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebcf57b17348d7bfa4a7ac81cbfacaf0d047cacc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
