6cf34278d1dfb387145e93d28710f987cf63f165
Shenny <teexes-pm23@student.tarc.edu.my>
2025-12-21 22:20:56 +0800

    Rental Test File Remove

--- Assignment/obj/Debug/net9.0/Assignment.AssemblyInfo.cs
+++ Assignment/obj/Debug/net9.0/Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b854f13dc7ef6c9200e214280f1822b1866a7cb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27cac9191ae6fd0dd8d34f8497202a3a9f46dbff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
