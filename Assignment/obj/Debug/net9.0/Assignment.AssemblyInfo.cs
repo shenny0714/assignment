b854f13dc7ef6c9200e214280f1822b1866a7cb6
Shenny <teexes-pm23@student.tarc.edu.my>
2025-12-21 22:07:45 +0800

    Update car catalog detail

--- Assignment/obj/Debug/net9.0/Assignment.AssemblyInfo.cs
+++ Assignment/obj/Debug/net9.0/Assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203e82a4eedf157521745fbf9547f7ae93f2991b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5310bb4212edd99ad9a5c386bf0b9c277a83b85")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
